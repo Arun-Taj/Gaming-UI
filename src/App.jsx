5fb0a0d48d67dbd64d606d8536543e6d15e0d0f6
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-07-24 10:40:40 +0545

    css maintained

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import HomePage from './Components/HomePage'
-import RegisterPage from './Components/RegisterPage'
+
 
 const App = () => {
   return (
     <div>
       <HomePage/>
-      {/* <RegisterPage/> */}
+    
     </div>
   )
 }
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     <>
       <div className="text-white min-h-screen bg-gradient-to-b from-[#151932] via-slate-900 to-[#151932] ">
         {/* navbar */}
-        <nav className="w-full top-0 p-4 flex flex-col md:flex-row justify-between items-center z-10 max-w-full px-20">
+        <nav className="w-full top-0 p-4 flex flex-col md:flex-row justify-between items-center z-10 max-w-full pl-36 pr-36">
           <div className="flex items-center space-x-8 mb-4 md:mb-0">
             <div className="text-2xl font-bold flex items-center space-x-2 cursor-pointer ">
               <FaGamepad size={30} />
@@ -37,33 +37,38 @@ const HomePage = () => {
           </div>
         </nav>
 
-        <div className="relative pb-64 flex flex-col text-white min-h-full">
-          {/* Background Image */}
+        <div className="relative flex flex-col text-white min-h-screen">
+          {/* Background Image relative flex flex-col text-white min-h-screen*/}
           <div
             className="absolute inset-0 bg-cover bg-center"
             style={{ backgroundImage: "url(/src/assets/clashOfClans.jpeg)" }}
           >
             {/* Overlay */}
-            <div className="absolute inset-0 bg-gradient-to-b from-slate-900 via-transparent to-slate-900"></div>
+            <div className="absolute inset-0 bg-gradient-to-b from-slate-900 via-slate-900/50 to-slate-900"></div>
           </div>
 
-          <div className="relative flex flex-col items-center mt-10 md:mt-20 text-center px-4">
-            <h1 className="text-4xl md:text-6xl font-extrabold">
-              GET READY TO PLAY
-            </h1>
+          <div className="relative flex flex-col  mt-10 md:mt-20 pl-36 pr-36  items-start">
+            <p className="text-4xl md:text-6xl font-extrabold">
+              GET READY TO 
+            </p>
+            <p className="text-4xl md:text-6xl font-extrabold">
+              PLAY 
+            </p>
+
             <p className="mt-4 text-2xl sm:text-3xl  text-[#3351E5] font-bold">
               Discover Games You Love
             </p>
-            <button className="mt-6 px-6 py-3 bg-blue-500 rounded-full text-sm md:text-lg hover:bg-blue-700 flex items-center">
-              Play Now <FaArrowRightLong className="ml-4" />
+            <button className="mt-6 px-4 py-2  bg-blue-500 rounded-full text-xs md:text-lg hover:bg-blue-700 flex items-center md:mr-96">
+              Play Now <FaArrowRightLong className="ml-2" />
             </button>
+            
           </div>
         </div>
 
-        <div className="pt-10 text-center w-full flex justify-center">
-          <h2 className="text-xl md:text-2xl font-bold">
+        <div className="pt-10 text-center w-full flex  pb-10 pl-36 pr-36">
+          <p className="text-xl md:text-2xl font-bold">
             Currently Trending Game
-          </h2>
+          </p>
         </div>
       </div>
     </>
