08764fa7a1b3ddf62c0822f83b9b930576232b19
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-07-24 13:58:51 +0545

    SignIn-page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HomePage from './Components/HomePage'
 import Signup from './Components/CreateAccount'
-
+import SignIn from './Components/SignInPage'
 
 const App = () => {
   return (
     <div>
       {/* <HomePage/> */}
     <Signup/>
+    {/* <SignIn/> */}
     </div>
   )
 }
--- src/Components/SignInPage.jsx
+++ src/Components/SignInPage.jsx
@@ -6,33 +6,39 @@ import { FaFacebook } from "react-icons/fa6";
 
 const SignUp = () => {
   return (
-    <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 space-x-0 md:space-x-10 mt-10 justify-center items-center">
+    <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 space-x-0 md:space-x-10 mt-10 justify-center items-center pb-16">
       {/* Left Section with Image and Text Overlay */}
       <div className="relative w-full md:w-96">
         {/* Background Image */}
         <img
-          src="src/assets/Astro.jpeg"
+          src="src/assets/Astro2.jpeg"
           className="w-full h-full rounded-2xl"
           alt="Astronaut"
         />
         {/* Text Overlay */}
-        <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 rounded-xl text-white py-4 px-6">
-          <p className="text-lg font-mono font-extrabold">
-            Explore new games and enjoy with your friends
-          </p>
+         
+        <div className="absolute top-0 left-0 right-0   text-white py-4 px-8">
           <div className="flex items-center space-x-4 mt-2">
             <IoGameController className="text-red-600" size={25}/>
             <p className="text-lg font-mono font-extrabold">
               GAMING <br /> GALAXY
             </p>
           </div>
+          <p className="text-lg font-mono font-extrabold">
+            Explore new games and enjoy with your friends
+          </p>
+          
+        </div>
+        <div className="absolute bottom-0 left-0 right-0   text-white py-4 px-6">
+           
+          
           <p className="mt-2">@GAMING GALAXY</p>
         </div>
       </div>
 
       {/* Right Section with Sign Up Form */}
       <div className="flex flex-col">
-        <h1 className="text-4xl pb-4 font-extrabold">CREATE AN ACCOUNT</h1>
+        <h1 className="text-4xl pb-4 font-extrabold">WELCOME BACK !</h1>
         <div className="space-y-4">
           <div className="space-x-4 items-center pb-4 flex flex-row">
             <button className="bg-slate-950 text-white rounded-3xl px-4 py-2 flex items-center ">
@@ -43,26 +49,8 @@ const SignUp = () => {
             </button>
           </div>
           <ul className="space-y-4">
-            <li className="flex flex-col">
-              <label htmlFor="name" className="font-bold">
-                NAME
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="bg-slate-300 p-3 rounded-3xl pl-4"
-              />
-            </li>
-            <li className="flex flex-col">
-              <label htmlFor="email" className="font-bold">
-                EMAIL OR PHONE
-              </label>
-              <input
-                type="text"
-                id="email"
-                className="bg-slate-300 p-3 rounded-3xl pl-4"
-              />
-            </li>
+           
+            
             <li className="flex flex-col">
               <label htmlFor="username" className="font-bold">
                 USERNAME
@@ -93,10 +81,10 @@ const SignUp = () => {
               Sign Up
             </button>
           </div>
-          <div className="mt-4 pb-6">
+          <div className="mt-4">
             <p>
               Already have an account?
-              <button className="cursor-pointer text-blue-600">Sign In</button>
+               <button className="cursor-pointer text-blue-600">Sign In</button>
             </p>
           </div>
         </div>
