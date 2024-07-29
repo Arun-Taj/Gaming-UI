a63d870d5244031223ddd32226683ca8119aeeb1
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-07-29 11:13:25 +0545

    Trying to navigate sidebar items

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,33 @@ import SignUp from './Components/SignUp';
 import SignIn from './Components/SignInPage';
 import DashBoard from './UserDashBoard/Components/DashBoard';
 import Sidebar from './UserDashBoard/Components/Sidebar';
-import { BrowserRouter as Router, Routes,Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Store from './UserDashBoard/Components/MenuItems/Store'
+import Achivements from './UserDashBoard/Components/MenuItems/Achivements';
+import Browser from './UserDashBoard/Components/MenuItems/Browser'
+import Library from './UserDashBoard/Components/MenuItems/Library'
+import History from './UserDashBoard/Components/MenuItems/History';
 
 
 
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// const routes = [
-//   {
-//     path: '/',
-//     element: <DashBoard />,
-//   },
-//   {
-//     path: '/signin',
-//     element: <SignIn />,
-//   },
-//   {
-//     path: '/signup',
-//     element: <SignUp />,
-//   },
- 
-
- 
-// ];
-
-// const router = createBrowserRouter(routes);
 
 const App = () => {
   return (
     <>
-    {/* <RouterProvider router={router} /> */}
+    
     <Router>
       
       <Routes>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/' element={<DashBoard/>}/>
+        <Route path='/' element={<DashBoard/>}>
+          <Route path='store' element={<Store />} />
+          <Route path='browse' element={<Browser />} />
+          <Route path='library' element={<Library />} />
+          <Route path='history' element={<History />} />
+          <Route path='achivements' element={<Achivements />} />
+        </Route>
       </Routes>
     </Router>
       
