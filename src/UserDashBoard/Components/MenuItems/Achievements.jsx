be12f767f09966d43c40df29ebb2c4402ab3186e
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-11-27 10:17:31 +0545

    Made responsive to acheivement

--- src/UserDashBoard/Components/MenuItems/Achievements.jsx
+++ src/UserDashBoard/Components/MenuItems/Achievements.jsx
@@ -1,53 +1,61 @@
-import React from 'react'
+import React from 'react';
 import { FaMedal } from "react-icons/fa";
+
+import Fc24 from '../../../assetsUserDashBoard/Fc24.jpeg';
+import Pubg from '../../../assetsUserDashBoard/Pubg.jpeg';
+import Fortnite from '../../../assetsUserDashBoard/Fortnite.jpeg';
+import ClashOfRoyals from '../../../assetsUserDashBoard/ClashOfRoyals.jpeg';
 
 const Achievements = () => {
   const acheive = [
     {
-      image: 'src/assetsUserDashBoard/Fc24.jpeg',
-      name: 'American Football',
+      image: Fc24,
+      name: 'Fc24',
       title: 'Excellent',
     },
     {
-      image: 'src/assetsUserDashBoard/Fc24.jpeg',
-      name: 'Clash Royal',
-      title: 'Excellentt',
+      image: ClashOfRoyals,
+      name: 'Clash Of Royal',
+      title: 'Excellent',
     },
     {
-      image: 'src/assetsUserDashBoard/Pubg.jpeg',
+      image: Pubg,
       name: 'Pubg',
       title: 'Excellent',
     },
     {
-      image: 'src/assetsUserDashBoard/Fortnite.jpeg',
+      image: Fortnite,
       name: 'Fortnite',
       title: 'Outstanding',
     },
   ];
 
   return (
-    <div className=''>
-      <p className='text-2xl font-mono font-bold'> Achievements</p>
-      <ul>
+    <div className="p-4 md:p-6 lg:p-8">
+      <p className="text-lg md:text-2xl lg:text-3xl font-mono font-bold mb-4 text-center">Achievements</p>
+      <ul className="space-y-4">
         {acheive.map((achievement, index) => (
-          <li key={index} className="mb-2 flex items-center justify-between  bg-red-500 p-3 rounded-lg text-white font-bold">
-            <div className='flex items-center space-x-2 w-1/2'>
+          <li 
+            key={index} 
+            className="flex items-center justify-between bg-red-500 p-3 md:p-4 rounded-lg text-white font-bold shadow-lg"
+          >
+            <div className="flex items-center space-x-3 w-1/2">
               <img
                 src={achievement.image}
                 alt={achievement.name}
-                className="w-8 h-8 rounded-full border-gray-300"
+                className="w-10 h-10 md:w-12 md:h-12 rounded-full border-2 border-gray-300"
               />
-              <p className=' text-[10px] md:text-lg'>{achievement.name}</p>
+              <p className="text-sm md:text-base lg:text-lg">{achievement.name}</p>
             </div>
-            <div className='flex items-center justify-start w-1/2 text-[10px] px-3'>
-              <p >{achievement.title}</p> 
+            <div className="flex items-center justify-start w-1/2 px-2 text-sm md:text-base lg:text-lg">
+              <p>{achievement.title}</p>
             </div>
-             <FaMedal />
+            <FaMedal className="text-lg md:text-xl lg:text-2xl" />
           </li>
         ))}
-      </ul> 
+      </ul>
     </div>
   );
-}
+};
 
 export default Achievements;
